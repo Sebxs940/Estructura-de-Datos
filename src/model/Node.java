437a954d5c38b31e418779bf1e0747db97e164f0
CristianCamargo04 <61638786+CristianCamargo04@users.noreply.github.com>
2025-04-06 10:58:33 -0500

    Feat: test example of flux arrayList (#1)

---
+++ src/model/Node.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Node {
+    private int value;
+    private Node next;
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+}
